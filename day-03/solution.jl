ee359af6efca7974e3230e368d317d0249299f9f
Damien Gonot <damien.gonot@gmail.com>
2022-12-03 11:14:41 -0500

    day 3 part 2

--- day-03/solution.jl
+++ day-03/solution.jl
@@ -3,7 +3,9 @@ using DelimitedFiles
 input = readdlm("input", String) |> f -> map(x -> x |> x -> split(x, "") |> x -> only.(x) |> x -> Iterators.partition(x, Int(length(x) / 2)) |> x -> collect(x), f) |> vec
 
 alpha_map = merge(Dict(zip('a':'z', 1:26)), Dict(zip('A':'Z', 27:52)))
-
 both = map(sack -> intersect(sack[1], sack[2])[1], input)
-
 println("Part 1: ", map(x -> alpha_map[x], both) |> sum)
+
+input2 = readdlm("input", String) |> f -> split.(f, "") |> f -> map(x -> only.(x), f) |> x -> Iterators.partition(x, 3) |> collect
+all_three = map(sack -> intersect(sack[1], sack[2], sack[3])[1], input2)
+println("Part 2: ", map(x -> alpha_map[x], all_three) |> sum)
