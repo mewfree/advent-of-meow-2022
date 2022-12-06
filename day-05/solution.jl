6f63475882ff1f995d87a4f27bb09eaac691e3e7
Damien Gonot <damien.gonot@gmail.com>
2022-12-05 22:35:13 -0500

    day 5 part 2

--- day-05/solution.jl
+++ day-05/solution.jl
@@ -14,12 +14,24 @@ stacks = hcat(raw_stacks...) |> s -> eachrow(s) |> s -> filter.(b -> b != "   ",
 
 steps = input[2] |> s -> split.(s, "\n") |> s -> filter(!isempty, s)
 
+p1_stacks = deepcopy(stacks)
+
 for step in steps
     (_, n, _, origin, _, destination) = split(step, " ")
     for _ in 1:parse(Int, n)
-        x = popfirst!(stacks[parse(Int, origin)])
-        pushfirst!(stacks[parse(Int, destination)], x)
+        x = popfirst!(p1_stacks[parse(Int, origin)])
+        pushfirst!(p1_stacks[parse(Int, destination)], x)
     end
 end
 
-println("Part 1: ", first.(stacks) |> join |> s -> replace(s, r"[^A-Z]" => ""))
+println("Part 1: ", first.(p1_stacks) |> join |> s -> replace(s, r"[^A-Z]" => ""))
+
+p2_stacks = deepcopy(stacks)
+
+for step in steps
+    (_, n, _, origin, _, destination) = split(step, " ")
+    x = splice!(p2_stacks[parse(Int, origin)], 1:parse(Int, n))
+    pushfirst!(p2_stacks[parse(Int, destination)], x...)
+end
+
+println("Part 2: ", first.(p2_stacks) |> join |> s -> replace(s, r"[^A-Z]" => ""))
