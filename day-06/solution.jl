a83af70c5a51771178bc7dca8810fe655dd1dc14
Damien Gonot <damien.gonot@gmail.com>
2022-12-06 09:04:41 -0500

    more compact day 6 with ;

--- day-06/solution.jl
+++ day-06/solution.jl
@@ -3,20 +3,16 @@ input = open(f -> read(f, String), filename) |> chomp
 
 l4 = fill("", 4)
 for (i, v) in input |> i -> split(i, "") |> enumerate
-    push!(l4, v)
-    popfirst!(l4)
+    push!(l4, v); popfirst!(l4)
     if (l4 |> l -> filter(!isempty, l) |> unique |> length) == 4
-        println("Part 1: ", i)
-        break
+        println("Part 1: ", i); break
     end
 end
 
 l14 = fill("", 14)
 for (i, v) in input |> i -> split(i, "") |> enumerate
-    push!(l14, v)
-    popfirst!(l14)
+    push!(l14, v); popfirst!(l14)
     if (l14 |> l -> filter(!isempty, l) |> unique |> length) == 14
-        println("Part 2: ", i)
-        break
+        println("Part 2: ", i); break
     end
 end
