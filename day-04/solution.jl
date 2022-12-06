557e20ef3389ca769ab550ff8d49bbffbfe834bc
Damien Gonot <damien.gonot@gmail.com>
2022-12-05 22:56:42 -0500

    day 4 use sample_input as default

--- day-04/solution.jl
+++ day-04/solution.jl
@@ -1,6 +1,7 @@
 using DelimitedFiles
 
-input = readdlm("input", ',', String) |> f -> map(x -> map(i -> parse(Int, i), split(x, "-")), f) |> eachrow |> collect
+filename = get(ARGS, 1, "sample_input")
+input = readdlm(filename, ',', String) |> f -> map(x -> map(i -> parse(Int, i), split(x, "-")), f) |> eachrow |> collect
 
 fully_contained = [(x[1][1] in x[2][1]:x[2][2] && x[1][2] in x[2][1]:x[2][2]) || (x[2][1] in x[1][1]:x[1][2] && x[2][2] in x[1][1]:x[1][2]) for x in input]
 
